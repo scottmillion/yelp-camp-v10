0c0c1966a632136e011862ecf68dc3bc2145666e
Scott Million <scottmillion@gmail.com>
2020-09-08 14:48:44 +0700

    Added v9

---
+++ routes/campgrounds.js
@@ -0,0 +1,61 @@
+const
+  express = require('express'),
+  router = express.Router(),
+  Campground = require("../models/campground");
+
+// CAMPGROUND INDEX
+router.get("/", (req, res) => {
+
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds/index", { campgrounds: allCampgrounds });
+    }
+  });
+});
+
+// CAMPGROUND CREATE 
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const description = req.body.description;
+  const author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  const newCampground = { name, image, description, author }
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
+// CAMPGROUND NEW 
+router.get("/new", isLoggedIn, (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// CAMPGROUND SHOW 
+router.get("/:id", isLoggedIn, (req, res) => {
+  Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds/show", { campground: foundCampground });
+    }
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+module.exports = router;
